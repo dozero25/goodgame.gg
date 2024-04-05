92cf36da15d372359ebac2e6e326b25341e0e33b
junhwa Hong <john960109@naver.com>
2024-04-05 16:06:36 +0900

    AdminUser 레포,API,컨트롤러,서비스,매퍼 작성

--- goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/repository/AdminRepository.java
+++ goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/repository/AdminRepository.java
@@ -1,5 +1,6 @@
 package fourjo.idle.goodgame.gg.repository;
 
+import fourjo.idle.goodgame.gg.web.dto.AdminUserSelectAll;
 import fourjo.idle.goodgame.gg.web.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,8 +10,13 @@ import java.util.List;
 public interface AdminRepository {
 
 
-    public int userUpdate(UserDto userDto);
-    public int userDelete(UserDto userDto);
+    public int AdminUserUpdate(UserDto dto);
+    public int AdminUserDelete(int user_index);
 
+    public List<UserDto> AdminUserSelectAll();
+
+    public List<UserDto> AdminUserSearchList(String user_nick);
+
+    public UserDto AdminUserSelectOne(int user_index);
 
 }
