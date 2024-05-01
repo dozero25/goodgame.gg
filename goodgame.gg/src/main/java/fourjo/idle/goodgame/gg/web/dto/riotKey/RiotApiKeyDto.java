b15c59fae5580d69f6db6cae708b0773ce0fadfc
Yoon Do Yeong <ehdud05264@naver.com>
2024-05-01 18:59:29 +0900

    git ignore 연습

--- goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/web/dto/riotKey/RiotApiKeyDto.java
+++ goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/web/dto/riotKey/RiotApiKeyDto.java
@@ -8,7 +8,4 @@ public class RiotApiKeyDto {
     private final String mykey = "RGAPI-71682ba1-889e-4a48-91ad-86d3f1230745";
     private final String serverUrl = "https://kr.api.riotgames.com";
     private final String severUrlAsia = "https://asia.api.riotgames.com";
-
-    private int n;
-
 }
