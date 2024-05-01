2b7bc23e768471d9546535ad256627eaca60736b
Yoon Do Yeong <ehdud05264@naver.com>
2024-05-01 23:38:02 +0900

    불필요한 코드 삭제

--- goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/Application.java
+++ goodgame.gg/src/main/java/fourjo/idle/goodgame/gg/Application.java
@@ -2,8 +2,7 @@ package fourjo.idle.goodgame.gg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-@ServletComponentScan
+
 @SpringBootApplication
 public class Application {
 
